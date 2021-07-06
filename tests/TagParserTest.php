58efd2aa712edf191417e38230d63b2eab03d041
CodingLukas <lukasimbm@gmail.com>
2021-07-06 08:30:54 +0300

    video 3 Setup the World

--- tests/TagParserTest.php
+++ tests/TagParserTest.php
@@ -6,43 +6,47 @@ use App\TagParser;
 
 class TagParserTest extends \PHPUnit\Framework\TestCase
 {
+    protected TagParser $parser;
+
+    protected function setUp(): void
+    {
+        $this->parser = new TagParser();
+    }
+
     public function test_it_parses_a_single_tag()
     {
-        $parser = new TagParser();
+        // Given - Arrange
+//        $parser = new TagParser();
 
-        $result = $parser->parse('personal');
+        // When - Act
+        $result = $this->parser->parse('personal');
         $expected = ['personal'];
 
+        // Then - Assert
         $this->assertSame($result, $expected);
     }
 
     public function test_it_parses_a_comma_separated_list_of_tags()
     {
-        $parser = new TagParser();
-
-        $result = $parser->parse('personal, money, family');
+        $result = $this->parser->parse('personal, money, family');
         $expected = ['personal', 'money', 'family'];
         $this->assertSame($result, $expected);
     }
 
     public function test_it_parses_a_pipe_separated_list_of_tags()
     {
-        $parser = new TagParser();
-
-        $result = $parser->parse('personal | money | family');
+        $result = $this->parser->parse('personal | money | family');
         $expected = ['personal', 'money', 'family'];
         $this->assertSame($result, $expected);
     }
 
     public function test_spaces_are_optional()
     {
-        $parser = new TagParser();
-
-        $result = $parser->parse('personal,money,family');
+        $result = $this->parser->parse('personal,money,family');
         $expected = ['personal', 'money', 'family'];
         $this->assertSame($result, $expected);
 
-        $result = $parser->parse('personal|money|family');
+        $result = $this->parser->parse('personal|money|family');
         $expected = ['personal', 'money', 'family'];
         $this->assertSame($result, $expected);
     }
